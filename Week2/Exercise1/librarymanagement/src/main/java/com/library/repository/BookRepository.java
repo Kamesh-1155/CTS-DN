5d0c05673975b577ca5f7377f7489df046ce61ff
Kamesh-1155 <gmkamesh11@gmail.com>
2026-06-30 21:33:23 +0530

    Completed Week 2 Exercise 1 and Exercise 2

---
+++ Week2/Exercise1/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package Week2.Exercise1.librarymanagement.src.main.java.com.library.repository;
+
+public class BookRepository {
+
+    public void displayRepository() {
+        System.out.println("Book Repository Bean Created Successfully!");
+    }
+
+}
---
+++ Week2/Exercise1/librarymanagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,9 @@
+package Week2.Exercise1.librarymanagement.src.main.java.com.library.service;
+
+public class BookService {
+
+    public void displayService() {
+        System.out.println("Book Service Bean Created Successfully!");
+    }
+
+}
