d576482ac3ca33c90fd6074cdf7e86c56bba3209
Kamesh-1155 <gmkamesh11@gmail.com>
2026-07-10 11:26:44 +0530

    Update data.sql

--- Week2/QueryMethods/query-methods/src/main/resources/data.sql
+++ Week2/QueryMethods/query-methods/src/main/resources/data.sql
@@ -1,5 +1,14 @@
-INSERT INTO employee VALUES (1,'Anand',60000,'2023-01-15');
-INSERT INTO employee VALUES (2,'Sachin',45000,'2024-03-10');
-INSERT INTO employee VALUES (3,'Sandeep',75000,'2022-08-01');
-INSERT INTO employee VALUES (4,'Rahul',38000,'2025-02-18');
-INSERT INTO employee VALUES (5,'Anjali',90000,'2021-11-25');
+INSERT INTO employee (id, name, salary, joining_date)
+VALUES (1, 'Anand', 60000, '2023-01-15');
+
+INSERT INTO employee (id, name, salary, joining_date)
+VALUES (2, 'Sachin', 45000, '2024-03-10');
+
+INSERT INTO employee (id, name, salary, joining_date)
+VALUES (3, 'Sandeep', 75000, '2022-08-01');
+
+INSERT INTO employee (id, name, salary, joining_date)
+VALUES (4, 'Rahul', 38000, '2025-02-18');
+
+INSERT INTO employee (id, name, salary, joining_date)
+VALUES (5, 'Anjali', 90000, '2021-11-25');
