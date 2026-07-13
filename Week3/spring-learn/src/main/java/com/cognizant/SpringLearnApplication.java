e4b01835cff220d9a8b776b822911c074cb4cd11
Kamesh-1155 <gmkamesh11@gmail.com>
2026-07-13 22:50:15 +0530

    Log Start and End

--- Week3/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
+++ Week3/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
@@ -6,14 +6,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 	public static void displayDate() {
+		LOGGER.info("START");
     	ApplicationContext context =new ClassPathXmlApplicationContext("date-format.xml");
     	SimpleDateFormat sdf =context.getBean("dateFormat", SimpleDateFormat.class);
-		System.out.println(sdf.format(new Date()));
+		Date date = new Date();
+		LOGGER.debug("Current Date : {}", sdf.format(date));
+		LOGGER.info("END");
 }
+	private static final Logger LOGGER =LoggerFactory.getLogger(SpringLearnApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
 		displayDate();
