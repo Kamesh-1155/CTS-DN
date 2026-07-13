adbdbcba1bca61afc3834cfe8f2ca02b0c4cf612
Kamesh-1155 <gmkamesh11@gmail.com>
2026-07-13 23:28:00 +0530

    Get Country By ID

--- Week3/spring-learn/src/main/java/com/cognizant/controller/CountryController.java
+++ Week3/spring-learn/src/main/java/com/cognizant/controller/CountryController.java
@@ -6,6 +6,7 @@ import com.cognizant.model.Country;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +29,17 @@ public class CountryController {
                 new ClassPathXmlApplicationContext("country.xml");
 
         return context.getBean("countryList", List.class);
+    }
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+        ApplicationContext context =new ClassPathXmlApplicationContext("country.xml");
+        List<Country> countries = context.getBean("countryList", List.class);
+        for (Country country : countries) {
+            if (country.getCode().equalsIgnoreCase(code)) {
+                return country;
+            }
+        }
+
+        return null;
     }
 }
