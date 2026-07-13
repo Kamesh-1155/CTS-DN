5c4498e7f180d3f340fa9a13256966b9006b0378
Kamesh-1155 <gmkamesh11@gmail.com>
2026-07-13 19:42:50 +0530

    date output

--- Week3/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
+++ Week3/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
@@ -1,13 +1,21 @@
 package com.cognizant;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLearnApplication {
-
+	public static void displayDate() {
+    	ApplicationContext context =new ClassPathXmlApplicationContext("date-format.xml");
+    	SimpleDateFormat sdf =context.getBean("dateFormat", SimpleDateFormat.class);
+		System.out.println(sdf.format(new Date()));
+}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
-	}
-
+		displayDate();
+}
 }
