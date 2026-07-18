113ad37a6b4f90ae8ccee30338b6fadb7241a126
Kamesh-1155 <gmkamesh11@gmail.com>
2026-07-18 18:51:56 +0530

    Finish

---
+++ Week5/counterapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CountPeople from "./Components/CountPeople";
+
+function App() {
+  return (
+    <div>
+      <CountPeople />
+    </div>
+  );
+}
+
+export default App;
