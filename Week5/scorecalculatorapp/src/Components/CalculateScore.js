e461f460789d4ebc23c122e0752358441ff1dd1d
Kamesh-1155 <gmkamesh11@gmail.com>
2026-07-18 10:13:06 +0530

    Finish

---
+++ Week5/scorecalculatorapp/src/Components/CalculateScore.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../Stylesheets/mystyle.css";
+
+function CalculateScore(props) {
+
+    const average = props.total / props.goal;
+
+    return (
+        <div className="score-box">
+            <h2>Student Details</h2>
+
+            <p><strong>Name:</strong> {props.name}</p>
+            <p><strong>School:</strong> {props.school}</p>
+            <p><strong>Total Marks:</strong> {props.total}</p>
+            <p><strong>Subjects:</strong> {props.goal}</p>
+            <p><strong>Average:</strong> {average}</p>
+        </div>
+    );
+}
+
+export default CalculateScore;
